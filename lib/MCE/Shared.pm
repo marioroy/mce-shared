8adf0c4e1705248266535355fdfbecce8ead49c4
Mario Roy <marioeroy@gmail.com>
2018-06-25 09:10:41 -0400

    1.837 release for CPAN.

--- lib/MCE/Shared.pm
+++ lib/MCE/Shared.pm
@@ -13,7 +13,7 @@ use 5.010001;
 
 no warnings qw( threads recursion uninitialized once );
 
-our $VERSION = '1.836';
+our $VERSION = '1.837';
 
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 ## no critic (Subroutines::ProhibitSubroutinePrototypes)
@@ -446,7 +446,7 @@ MCE::Shared - MCE extension for sharing data supporting threads and processes
 
 =head1 VERSION
 
-This document describes MCE::Shared version 1.836
+This document describes MCE::Shared version 1.837
 
 =head1 SYNOPSIS
 
--- lib/MCE/Shared/Server.pm
+++ lib/MCE/Shared/Server.pm
@@ -13,7 +13,7 @@ no warnings qw( threads recursion uninitialized numeric once );
 
 package MCE::Shared::Server;
 
-our $VERSION = '1.836';
+our $VERSION = '1.837';
 
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 ## no critic (Subroutines::ProhibitExplicitReturnUndef)
@@ -1841,7 +1841,7 @@ MCE::Shared::Server - Server/Object packages for MCE::Shared
 
 =head1 VERSION
 
-This document describes MCE::Shared::Server version 1.836
+This document describes MCE::Shared::Server version 1.837
 
 =head1 DESCRIPTION
 
